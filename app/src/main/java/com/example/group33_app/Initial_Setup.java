b41555c8df77afbfd86fc5a56fe761e36100fbe4
MGorman24 <56391406+MGorman24@users.noreply.github.com>
2020-01-29 18:11:55 +0000

    Imported startup and initial setup screens

---
+++ app/src/main/java/com/example/group33_app/Initial_Setup.java
@@ -0,0 +1,14 @@
+package com.example.group33_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Initial_Setup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial__setup);
+    }
+}
